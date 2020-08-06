3da590fc729caacfaef39a2066578fde9735441d
Kenny Sun <kenneth.sun1204@gmail.com>
2020-08-06 14:16:13 -0700

    finalized project

---
+++ NPS_Site/SweetMap/static/config.js
@@ -0,0 +1,5 @@
+// API key
+//const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ"; Testing key
+//const API_KEY = "pk.eyJ1IjoibW9zdGRpZmZpY3VsdCIsImEiOiJja2JiamxwbTQwMXhsMnNwaXVobGR5aGY0In0.2FfocTGJZ_YVIGGtUmmTYA"; Public
+const API_KEY = "pk.eyJ1IjoibW9zdGRpZmZpY3VsdCIsImEiOiJja2JianFmM28wMXdvMnhyeWl4M2E3dGhnIn0.3xvnLC6gTvclLiJ2S260rg";
+
